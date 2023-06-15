6ea89e3cb576a6c7c53ae938d8457974166976e8
Shuyibe48 <gthrough48@gmail.com>
2023-06-15 19:02:40 +0600

    set user by default role student

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,6 +2,7 @@
 export const saveUser = user => {
     const currentUser = {
         email: user.email,
+        role: 'student',
     }
 
     fetch(`${import.meta.env.VITE_API_URL}/users/${user?.email}`, {
